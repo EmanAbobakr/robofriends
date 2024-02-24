d40c1f1a5cac278579142d901f7c71298fcccb2c
eman.abobakr <abobakremy@gmail.com>
2024-02-24 02:13:44 +0200

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import "./App.css";
 import { CardList } from "./CardList";
-import { robots } from "./robots";
 import { SearchBox } from "./SearchBox";
-import { useState } from "react";
+import { Component } from "react";
 
-export const App = () => {
-  const [searchedWord, setSearchedWord] = useState("");
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = { robots: [], searchedWord: "" };
+  }
+  searchHandler = (event) => {
+    this.setState({ searchedWord: event.target.value });
+  };
 
-  function searchHandler(name) {
-    setSearchedWord(name.target.value);
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((result) => result.json())
+      .then((robots) => this.setState({ robots: robots }));
   }
 
-  return (
-    <div className="tc">
-      <h1 className="f1">Robo Friends</h1>
-      <SearchBox setSearchedWord={searchHandler} />
-      <CardList
-        robots={robots.filter((robot) => {
-          return robot.name.toLowerCase().includes(searchedWord.toLowerCase());
-        })}
-      />
-    </div>
-  );
-};
+  render() {
+    const { robots, searchedWord } = this.state;
+    return !robots.length ? (
+      <h1 className="tc f1">Loading</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">Robo Friends</h1>
+        <SearchBox setSearchedWord={this.searchHandler} />
+        <CardList
+          robots={robots.filter((robot) => {
+            return robot.name
+              .toLowerCase()
+              .includes(searchedWord.toLowerCase());
+          })}
+        />
+      </div>
+    );
+  }
+}
